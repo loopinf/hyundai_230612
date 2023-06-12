9e32074b27811a4c28b902034b5bfe58e8f32b90
Chansik Yun <chansigi@gmail.com>
2023-06-12 10:49:45 +0900

    .

--- 2_namespace3.cpp
+++ 2_namespace3.cpp
@@ -9,10 +9,28 @@
 #include <cstring>
 #include <cctype>
 
+/*
 int main()
 {
     std::printf("Hello, C\n");
     // C++에서 제공하는 C의 모든 함수는 std 이름 공간 안에서 사용할 수 있습니다.
 
     std::cout << "Hello, C++" << std::endl;
+}
+*/
+
+#include <algorithm>
+// std::count 함수가 이 안에 존재합니다.
+
+// using namespace std;
+using std::cout;
+using std::endl;
+
+int count = 0;
+
+int main()
+{
+    count = 100;
+
+    cout << "Hello, C++" << endl;
 }
