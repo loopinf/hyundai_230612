c602ccecbc262eeb156d3654a0a11f838df869e0
Chansik Yun <chansigi@gmail.com>
2023-06-12 10:25:29 +0900

    .

--- 2_namespace.cpp
+++ 2_namespace.cpp
@@ -34,6 +34,7 @@ void Init() { std::cout << "Video Init" << std::endl; }
 // 2. namespace
 // - 이름 충돌 해결
 // - 프로그램의 각 요소를 관련된 요소끼리 그룹화할 수 있습니다.
+// - C++의 표준 라이브러리는 std 이름 공간안에 존재합니다.
 
 int main()
 {
