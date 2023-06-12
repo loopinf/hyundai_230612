eab4306e1a064fed303c497363e7d877fba9a07e
Chansik Yun <chansigi@gmail.com>
2023-06-12 11:42:20 +0900

    .

--- 4_변수2.cpp
+++ 4_변수2.cpp
@@ -7,6 +7,7 @@ struct Point {
     int y;
 };
 
+#if 0
 int main()
 {
     // Copy Initialization(복사 초기화)
@@ -18,4 +19,39 @@ int main()
     int n2(42);
     int x2[3] { 1, 2, 3 };
     Point pt2 { 10, 20 };
+
+    // C++11 - Uniform Initialization(일관된 초기화)
+    // - Copy Initialization
+    int n3 = { 42 };
+    int x3[3] = { 1, 2, 3 };
+    Point pt3 = { 10, 20 };
+
+    // - Direct Initialization
+    int n4 { 42 };
+    int x4[3] { 1, 2, 3 };
+    Point pt4 { 10, 20 };
+}
+#endif
+
+// Uniform Initialization
+// => Preventing Narrow
+
+int main()
+{
+    double d = 3.14;
+    // int n = d;
+
+    // 실수 타입이 정수 타입으로의 암묵적인 변환을 허용하지 않습니다.
+    // int n = { (int)d };
+
+    // int n(d);
+    int n { (int)d };
+    cout << n << endl;
+
+    // 더 큰 표현 범위를 작은 표현 범위의 정수로의 암묵적인 변환을 허용하지 않습니다.
+    long long ll = 420000000000LL; // 64비트 정수 타입입니다.
+    // int n2 = ll;
+    int n2 = { (int)ll };
+
+    cout << n2 << endl;
 }
