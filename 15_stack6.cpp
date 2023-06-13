61f4439db22c89dbfe9b782e9c79b2741e41d418
Chansik Yun <chansigi@gmail.com>
2023-06-13 14:28:48 +0900

    .

---
+++ 15_stack6.cpp
@@ -0,0 +1,56 @@
+// 15_stack6.cpp
+#include <iostream>
+using namespace std;
+
+// 문제점
+// - 사용자가 원하는 크기의 스택을 생성하고 싶습니다.
+
+// 해결방법
+// - 생성자의 인자를 통해 크기를 전달하면 됩니다.
+// - 생성자는 오버로딩이 가능합니다.
+//   : 객체를 생성하는 다양한 옵션을 제공할 수 있습니다.
+
+class Stack {
+    int* buff;
+    int top;
+
+public:
+    Stack()
+    {
+        cout << "Stack()" << endl;
+        buff = new int[10];
+        top = 0;
+    }
+
+    Stack(int size)
+    {
+        cout << "Stack(int)" << endl;
+        buff = new int[size];
+        top = 0;
+    }
+
+    void push(int n)
+    {
+        buff[top++] = n;
+    }
+
+    int pop()
+    {
+        return buff[--top];
+    }
+};
+
+int main()
+{
+
+    Stack s1(100); // Stack(int)
+    Stack s2; // Stack()
+
+    s1.push(10);
+    s1.push(20);
+    s1.push(30);
+
+    cout << s1.pop() << endl;
+    cout << s1.pop() << endl;
+    cout << s1.pop() << endl;
+}
