c39c02587bcbc9ed6f6fce4002cb81cbd79f36da
Chansik Yun <chansigi@gmail.com>
2023-06-14 13:48:35 +0900

    .

--- 21_복사생성자2.cpp
+++ 21_복사생성자2.cpp
@@ -15,6 +15,7 @@ public:
 // 해결 방법
 //  => const&로 전달해야 합니다.
 // void foo(Sample s)
+#if 0
 void foo(const Sample& s)
 {
 }
@@ -28,3 +29,22 @@ int main()
     cout << "----------" << endl;
 }
 // ~Sample();
+#endif
+
+// 2) 함수가 객체를 값으로 반환할 때
+//  > 유효한 메모리의 객체라면, 참조를 반환하면 됩니다.
+
+Sample s;
+// Sample foo()
+
+Sample& foo()
+{
+    return s; // '반환용 임시 객체'가 생성되서 전달됩니다.
+}
+
+int main()
+{
+    cout << "--------" << endl;
+    foo();
+    cout << "--------" << endl;
+}
