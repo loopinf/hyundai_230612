256c2aafd1e0a1966bae16e8aaf4aefe6e672ea6
Chansik Yun <chansigi@gmail.com>
2023-06-14 15:14:17 +0900

    .

---
+++ 23_객체복사.cpp
@@ -0,0 +1,35 @@
+// 23_객체복사.cpp
+#include <iostream>
+using namespace std;
+
+class User {
+    char* name;
+    int age;
+
+public:
+    User(const char* s, int n)
+        : age(n)
+    {
+        name = new char[strlen(s) + 1];
+        strcpy(name, s);
+    }
+
+    ~User()
+    {
+        delete[] name;
+    }
+
+    void Print()
+    {
+        cout << name << ", " << age << endl;
+    }
+};
+
+int main()
+{
+    User user("Tom", 42);
+    user.Print();
+
+    User other = user;
+    other.Print();
+}
