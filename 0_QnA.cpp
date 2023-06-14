ff24a04f35f4b916d634078a9ae89f2c6e2b7e49
Chansik Yun <chansigi@gmail.com>
2023-06-14 15:48:47 +0900

    .

--- 0_QnA.cpp
+++ 0_QnA.cpp
@@ -26,3 +26,25 @@ int main()
 
     PrintPeron(&person);
 }
+
+#if 0
+#include <string>
+class Sample {
+    string name;
+
+public:
+    Sample(const string& s)
+        : name(s)
+    {
+        cout << "객체 생성: " << name << endl;
+    }
+
+    ~Sample() { cout << "객체 파괴: " << name << endl; }
+};
+
+int main()
+{
+    Sample s1("A");
+    Sample s2("B");
+}
+#endif
