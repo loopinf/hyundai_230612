695ea372bc0291c201176813a7138a5d9c8ae26c
Chansik Yun <chansigi@gmail.com>
2023-06-14 14:22:26 +0900

    .

---
+++ 21_복사생성자4.cpp
@@ -0,0 +1,35 @@
+// 21_복사생성자4.cpp
+#include <iostream>
+using namespace std;
+
+template <typename TYPE>
+class Stack {
+    TYPE* buff;
+    int top;
+
+public:
+    Stack(int size = 10)
+    {
+        buff = new TYPE[size];
+        top = 0;
+    }
+
+    ~Stack()
+    {
+        delete[] buff;
+    }
+
+    void push(TYPE n)
+    {
+        buff[top++] = n;
+    }
+
+    TYPE pop()
+    {
+        return buff[--top];
+    }
+};
+
+int main()
+{
+}
