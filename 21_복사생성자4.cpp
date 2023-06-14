877770f971ffd863fc14b035b81072bfc319f33c
Chansik Yun <chansigi@gmail.com>
2023-06-14 14:23:55 +0900

    .

--- 21_복사생성자4.cpp
+++ 21_복사생성자4.cpp
@@ -2,6 +2,14 @@
 #include <iostream>
 using namespace std;
 
+class Point {
+public:
+    Point(int x, int y) { }
+
+    Point(const Point& rhs) { cout << "Point(const Point&)" << endl; }
+    ~Point() { cout << "~Point()" << endl; }
+};
+
 template <typename TYPE>
 class Stack {
     TYPE* buff;
