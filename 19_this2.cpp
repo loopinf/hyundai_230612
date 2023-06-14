036e840310875d7778484ba9a0e3f9d87e2b8891
Chansik Yun <chansigi@gmail.com>
2023-06-14 11:47:27 +0900

    .

--- 19_this2.cpp
+++ 19_this2.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 using namespace std;
 
+#if 0
 class Sample {
 public:
     Sample() { cout << "Sample()" << endl; }
@@ -31,3 +32,32 @@ int main()
     // delete p;
 
 } // Sample::~Sample()
+#endif
+
+// private 생성자
+//  : 객체 생성의 정책을 제한하고 싶을 때
+class Sample {
+private:
+    Sample() { cout << "Sample()" << endl; }
+
+public:
+    ~Sample() { cout << "~Sample()" << endl; }
+
+    friend Sample* CreateSample();
+};
+
+// Sample 객체는 CreateSample 함수를 통해서만 생성할 수 있습니다.
+Sample* CreateSample()
+{
+    return new Sample;
+}
+
+// Sample s;
+int main()
+{
+    // Sample s2;
+    // Sample* p = new Sample;
+}
+
+// Design Pattern: 설계 패턴
+// => 잘 만들어진 소프트웨어의 설계적 공통점 23가지
