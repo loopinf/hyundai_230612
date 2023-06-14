f8ab3662c467543dc450c2c38039af125411547b
Chansik Yun <chansigi@gmail.com>
2023-06-14 14:33:54 +0900

    .

--- 21_복사생성자4.cpp
+++ 21_복사생성자4.cpp
@@ -7,34 +7,53 @@ public:
     Point() { }
 
     Point(const Point& rhs) { cout << "Point(const Point&)" << endl; }
-    ~Point() { cout << "~Point()" << endl; }
+    // ~Point() { cout << "~Point()" << endl; }
 };
 
 template <typename TYPE>
 class Stack {
-    TYPE* buff;
-    int top;
+    TYPE* buff_;
+    int top_;
 
 public:
     Stack(int size = 10)
     {
-        buff = new TYPE[size];
-        top = 0;
+        buff_ = new TYPE[size];
+        top_ = 0;
     }
 
     ~Stack()
     {
-        delete[] buff;
+        delete[] buff_;
     }
 
-    void push(TYPE n)
+    // void push(TYPE n)           => void push(Point n)
+    //                             => void push(int n)
+
+    // void push(const TYPE& n)    => void push(const Point& n)
+    //                                void push(const int& n)
+    void push(const TYPE& n)
     {
-        buff[top++] = n;
+        buff_[top_++] = n;
     }
 
+// 아래 함수는 참조를 반환할 수 없습니다.
+// 스택에서 제거되는 영역에 대한 참조를 반환하면 안됩니다.
+//  - 해결 방법: 참조하는 연산과 제거하는 연산을 분리해야 합니다.
+#if 0
     TYPE pop()
     {
-        return buff[--top];
+        return buff_[--top_];
+    }
+#endif
+    TYPE& top()
+    {
+        return buff_[top_ - 1];
+    }
+
+    void pop()
+    {
+        --top_;
     }
 };
 
@@ -46,5 +65,10 @@ int main()
 
     cout << "-----------" << endl;
     s.push(pt);
+    cout << "-----------" << endl;
+
+    cout << "-----------" << endl;
+    s.top();
+    s.pop();
     cout << "-----------" << endl;
 }
