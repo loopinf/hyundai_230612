d5025551d9dd136b65502e99e2aadaad223ea8b0
Chansik Yun <chansigi@gmail.com>
2023-06-15 13:42:43 +0900

    .

--- 28_다형성2.cpp
+++ 28_다형성2.cpp
@@ -29,6 +29,8 @@ void PrintAge(const Cat& cat)
 }
 #endif
 
+class Rabbit : public Animal { };
+
 // 1. 동종을 처리하는 함수
 //  : Animal을 상속받는 모든 자식 클래스의 객체는 해당 함수를 이용할 수 있습니다.
 void PrintAge(const Animal& animal)
@@ -36,11 +38,22 @@ void PrintAge(const Animal& animal)
     cout << animal.GetAge() << endl;
 }
 
+void PrintAge(const Animal* p)
+{
+    cout << p->GetAge() << endl;
+}
+
 int main()
 {
     Dog d;
     PrintAge(d);
+    PrintAge(&d);
 
     Cat c;
     PrintAge(c);
+    PrintAge(&c);
+
+    Rabbit r;
+    PrintAge(r);
+    PrintAge(&r);
 }
