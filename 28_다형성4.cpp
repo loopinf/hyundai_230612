7dfba546130c65231367ba71454d58d076416826
Chansik Yun <chansigi@gmail.com>
2023-06-15 14:17:36 +0900

    .

--- 28_다형성4.cpp
+++ 28_다형성4.cpp
@@ -7,14 +7,44 @@ using namespace std;
 
 class Car {
 public:
-    void Start() const { cout << "Car::Start()" << endl; }
+    virtual void Start() const { cout << "Car::Start()" << endl; }
 };
 
 class Sedan : public Car {
 public:
-    void Start() const { cout << "Sedan::Start()" << endl; }
+    virtual void Start() const { cout << "Sedan::Start()" << endl; }
 };
 
+// 2. 함수 바인딩(Binding)
+//   p->Start();
+// 1. 정적 바인딩(static binding)
+//   : 컴파일 타임에 컴파일러가 결정합니다.
+//     컴파일러가 컴파일 타임에 p의 타입을 보고 어떤 함수를 호출할지 결정합니다.
+
+// 2. 동적 바인딩(dynamic binding)
+//   : 실행 시간에 결정합니다.
+//     컴파일러가 p가 실행중에 어떤 타입의 객체를 가지고 있는지 조사해서
+//     함수를 호출하는 코드를 삽입합니다.
+
+int main()
+{
+    Car car;
+    Sedan sedan;
+
+    Car* p = &car;
+    p->Start();
+
+    p = &sedan;
+    p->Start();
+
+    Car& r1 = car;
+    r1.Start();
+
+    Car& r2 = sedan;
+    r2.Start();
+}
+
+#if 0
 int main()
 {
     Car car;
@@ -23,3 +53,4 @@ int main()
     Sedan sedan;
     sedan.Start();
 }
+#endif
