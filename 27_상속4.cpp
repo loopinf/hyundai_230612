b2c3a12dc5bab173a09f34fd3edcaf4b9e7d2894
Chansik Yun <chansigi@gmail.com>
2023-06-15 13:09:49 +0900

    .

---
+++ 27_상속4.cpp
@@ -0,0 +1,35 @@
+// 27_상속4.cpp
+#include <iostream>
+using namespace std;
+
+class Base {
+public:
+    Base() { cout << "Base()" << endl; }
+    ~Base() { cout << "~Base()" << endl; }
+};
+
+class Derived : public Base {
+public:
+    Derived()
+    // : Base()    // 2) 초기화 리스트를 통해 부모의 기본 생성자가 호출됩니다. - Base::Base()
+    {
+        cout << "Derived()" << endl;
+    }
+
+    ~Derived()
+    {
+        cout << "~Derived()" << endl;
+    } // 2> 자식의 소멸자의 마지막에 부모의 소멸자를 호출합니다. - Base::~Base()
+};
+
+/*
+Base()
+Derived()
+~Derived()
+~Base()
+*/
+
+int main()
+{
+    Derived d; // 1) Derived::Derived()
+} // 1> Derived::~Derived()
