caac18e082fdbee8a8288b000e15de85ed974e93
Chansik Yun <chansigi@gmail.com>
2023-06-15 15:42:00 +0900

    .

--- 28_다형성8.cpp
+++ 28_다형성8.cpp
@@ -16,6 +16,11 @@ using namespace std;
 // 4. 자식이 재정의하는 모든 함수는 반드시 "가상 함수"이어야 합니다.
 // 5. 부모 클래스의 소멸자는 반드시 가상이어야 합니다.
 
+// 6. OCP(Open Close Principle, 개방 폐쇄 원칙)
+//    => 확장에는 열려 있어야 하고,(새로운 기능 추가되어도)
+//       수정에는 닫혀 있어야 합니다.(기존 코드는 수정되면 안된다)
+//    "다형성(polymorphism)"은 OCP를 만족합니다.
+
 class Shape {
 public:
     virtual ~Shape() { } // !!!!!
@@ -33,6 +38,11 @@ public:
     void Draw() const override { cout << "Draw Circle" << endl; }
 };
 
+class Triangle : public Shape {
+public:
+    void Draw() const override { cout << "Triangle Circle" << endl; }
+};
+
 int main()
 {
     // vector<Rect*> rects;
@@ -48,10 +58,17 @@ int main()
             shapes.push_back(new Rect);
         } else if (cmd == 2) {
             shapes.push_back(new Circle);
-        } else if (cmd == 9) {
+        } else if (cmd == 3) {
+            shapes.push_back(new Triangle);
+        }
+
+        else if (cmd == 9) {
 
             for (auto e : shapes) {
                 e->Draw();
+                // e->Draw()   ---> Rect  -> Rect::Draw
+                //                 Circle -> Circle::Draw
+                //                  ....
             }
         } else {
             break;
