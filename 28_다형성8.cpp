e9bebb8ce4611a4305da674dc9b5e7e7375a289f
Chansik Yun <chansigi@gmail.com>
2023-06-15 15:32:37 +0900

    .

---
+++ 28_다형성8.cpp
@@ -0,0 +1,65 @@
+// 28_다형성.cpp
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// 1. 각 도형의 클래스를 설계합니다.
+// 2. 부모 클래스 도입
+//  => 하나의 컨테이너에 모든 도형을 저장할 수 있습니다.
+
+// 3. 자식 클래스의 공통의 기능을 부모 클래스의 포인터(참조)를 통해 이용하기 위해서는
+//    해당 기능이 반드시 부모로부터 비롯되어야 합니다.
+//    : LSP(Liskov Substitution Principle, 리스코프 치환 원칙)
+//    => 자식 클래스는 부모 클래스로 대체할 수 있어야 한다.
+//      "자식의 공통의 기능은 부모 클래스로부터 비롯되어야 한다."
+
+// 4. 자식이 재정의하는 모든 함수는 반드시 "가상 함수"이어야 합니다.
+// 5. 부모 클래스의 소멸자는 반드시 가상이어야 합니다.
+
+class Shape {
+public:
+    virtual ~Shape() { } // !!!!!
+
+    virtual void Draw() const { cout << "Draw Shape" << endl; }
+};
+
+class Rect : public Shape {
+public:
+    void Draw() const override { cout << "Draw Rect" << endl; }
+};
+
+class Circle : public Shape {
+public:
+    void Draw() const override { cout << "Draw Circle" << endl; }
+};
+
+int main()
+{
+    // vector<Rect*> rects;
+    // vector<Circle*> circles;
+
+    vector<Shape*> shapes; // 동종을 보관하는 컨테이너
+
+    while (1) {
+        int cmd;
+        cin >> cmd;
+
+        if (cmd == 1) {
+            shapes.push_back(new Rect);
+        } else if (cmd == 2) {
+            shapes.push_back(new Circle);
+        } else if (cmd == 9) {
+
+            for (auto e : shapes) {
+                e->Draw();
+            }
+        } else {
+            break;
+        }
+    }
+
+    // 모든 도형의 메모리를 해지합니다.
+    for (auto e : shapes) {
+        delete e;
+    }
+}
