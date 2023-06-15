a373af5645403a4558f062d2877011f1b99f8d2a
Chansik Yun <chansigi@gmail.com>
2023-06-15 14:37:19 +0900

    .

---
+++ 28_다형성5.cpp
@@ -0,0 +1,34 @@
+// 28_다형성5.cpp
+#include <iostream>
+using namespace std;
+
+// 1) 반드시 부모의 멤버 함수가 가상함수 이어야 합니다.
+//    가상 함수가 아닌 멤버 함수는 오버라이딩 하지 않습니다.
+
+// 2) virtual
+//  - 부모의 멤버 함수가 가상함수이면, 자식도 가상함수입니다.
+
+// 3) 문제점
+//  1) 부모의 함수의 이름을 잘못 사용하였을 때를 알 수 없습니다.
+//  2) 반드시 가상 함수에 대해서만 오버라이딩 해야 합니다.
+//     그래야 부모의 포인터(참조) 타입을 통해 해당 기능을 이용할 수 있습니다.
+//  => C++11, override
+
+class Car {
+public:
+    virtual void Start() const { cout << "Car::Start()" << endl; }
+};
+
+class Sedan : public Car {
+public:
+    // virtual void Start() const { cout << "Sedan::Start()" << endl; }
+    void Start() const override { cout << "Sedan::Start()" << endl; }
+};
+
+int main()
+{
+    Sedan s;
+    Car* p = &s;
+
+    p->Start();
+}
